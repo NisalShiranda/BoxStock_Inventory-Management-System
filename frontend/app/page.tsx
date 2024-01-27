bf689d2452c06e57ab9774b4a82a886e8ee57fd0
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-01-27 22:46:36 +0530

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,24 +8,35 @@ export default function Home() {
     <>
     <div><Navbar /></div>
   
-    <div className=" bg-[#EEEBEB]  ">
-      <div className="md:flex md:flex-row mx-[200px] py-10">
-        <div className="flex flex-col justify-center items-center">
-          <div className="text-6xl font-semibold ">A Smarter Way To Manage Your Inventory.</div>
-          <div className="pt-2">The best inventory software for small businesses to manage 
-                inventory, supplies, and everything else.
+    <div className=" bg-[#EEEBEB] ">
+      <div className="py-5 mx-6 md:flex flex-row md:justify-center md:items-center md:mx-[200px]">
+        <div className="">
+          <div className="md:flex justify-center items-center">
+            <p className="text-center text-4xl font-semibold md:text-left md:text-6xl">A <span className="text-[#FCA311] ">Smarter </span>Way To Manage Your Inventory</p>
+          </div>
+          <div>
+            <p className="text-center pt-3 text-sm md:text-left">The best inventory software for small businesses to manage 
+            inventory, supplies, and everything else.</p>
+          </div>
+          <div className="pt-3 flex flex-col md:flex md:flex-row ">
+            <div className="flex justify-center">
+              <button className="bg-[#14213D]  text-white p-2 rounded-lg hover:bg-[#FCA311] hover:text-[#14213D] text-sm">Try It Free</button>
+            </div>
+            <div className="flex justify-center pt-2 text">
+              <p className="text-sm underline decoration-solid md:ml-5">See All Plans</p>
+            </div>
+            
           </div>
         </div>
-        <div>
-        <Image className=''
-          src="/hero1.png"
-          width={800}
-          height={800}
-          alt="Picture of the author"
-        />
-        </div>
+        <div className="flex justify-center ">
+          <img className="w-[1000px] "src="/hero1.png"></img>
+        
+        
+        
 
+        </div>
       </div>
+      
 
     </div>
     
