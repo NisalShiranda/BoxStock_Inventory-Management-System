dbb4c5566119640f800c6d0b983c3707cd065ae6
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-01-27 13:04:03 +0530

    NavBar 1

--- frontend/app/(components)/navbar.tsx
+++ frontend/app/(components)/navbar.tsx
@@ -1,20 +1,14 @@
-import Image from "next/image";
-import slogan from "../public/Slogan.png"
+import React from 'react'
 
-export default function Home() {
+function navbar() {
   return (
     <>
-    <div className=" bg-white py-3  min-[860px]:flex min-[860px]:justify-between text-center min-[860px]:items-center md:mx-10 ">
+    <div className=" bg-white py-3  md:flex md:justify-between md:text-center md:items-center md:mx-10 ">
         <div className="flex justify-center">
-        <Image
-          src={slogan}
-          width={150}
-          height={150}
-          alt="Picture of the author"
-        />
+            <img className="w-40" src="../images/Slogan.png"></img>
         </div>
         <div>
-            <ul className=" justify-center text-center py-3 cursor-pointer  min-[860px]:flex flex-row md:space-x-5">
+            <ul className=" justify-center text-center py-3 cursor-pointer  md:flex flex-row md:space-x-5">
                 <li className="text-md py-3 hover:text-[#FCA311] duration-300 ">Product Tour</li>
                 <li className="text-md py-3 hover:text-[#FCA311] duration-300">Pricing</li>
                 <li className="text-md py-3 hover:text-[#FCA311] duration-300">Industries</li>
@@ -24,7 +18,7 @@ export default function Home() {
         </div>
 
         <div>
-            <ul className=" justify-center text-center cursor-pointer min-[860px]:flex flex-row md:space-x-5">
+            <ul className=" justify-center text-center cursor-pointer md:flex flex-row md:space-x-5">
                 <li className="text-md py-3 hover:text-[#FCA311] duration-300">Log In</li>
                 <li className="text-md py-1 flex justify-center"><button className="bg-[#14213D]  text-white p-2 rounded-lg hover:bg-[#FCA311] hover:text-[#14213D]">Try It Free</button></li>
             </ul>
@@ -33,6 +27,9 @@ export default function Home() {
     
     
     
+    
     </>
-  );
+  )
 }
+
+export default navbar
