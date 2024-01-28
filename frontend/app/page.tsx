24cd75e4265884092254683ea0f420433584f06d
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-01-28 15:01:34 +0530

    step section image changed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -33,7 +33,12 @@ export default function Home() {
         </div>
         {/* Hero Picture */}
         <div className="flex justify-center items-center md:flex md:justify-center md:items-center md:w-[50%]" >
-          <img src="/hero1.png"></img>
+        <Image className='pt-1 w-[80%]'
+          src="/hero1.png"
+          width={100000}
+          height={10000}
+          alt="Picture of the author"
+        />
         </div>
 
       </div>
@@ -50,7 +55,12 @@ export default function Home() {
       {/* step 01 word section */}
 
       <div className="flex justify-center items-center lg:w-[50%]">
-        <img className="w-[50%]"src="hero1.png"></img>
+      <Image className='pt-1 w-[60%]'
+          src="/step1.png"
+          width={1000}
+          height={1000}
+          alt="Picture of the author"
+        />
       </div>
 
       {/* step 01 image section */}
@@ -67,7 +77,7 @@ export default function Home() {
 
   {/* Step 02 Section */}
   <div className="mx-5 py-5 md:mx-36 lg:flex lg:flex-row">
-      {/* step 01 word section */}
+      {/* step 02 word section */}
       <div className="lg:w-[50%] lg:flex lg:flex-col lg:justify-center lg:items-center">
         <div>
           <p className="text-center text-xl font-semibold pt-5">Step 02</p>
@@ -78,11 +88,92 @@ export default function Home() {
         </div>
       </div>
 
+      {/* step 02 image section */}
+      <div className="flex justify-center items-center lg:w-[50%]">
+      <Image className='pt-1 w-[80%]'
+          src="/step1.png"
+          width={10000}
+          height={10000}
+          alt="Picture of the author"
+        />
+      </div>
+      
+    </div>
+
+    {/* Step 03 Section */}
+    <div className="mx-5 py-5 md:mx-36 lg:flex lg:flex-row">
+      {/* step 01 word section */}
+
+      <div className="flex justify-center items-center lg:w-[50%]">
+        <img className="w-[50%]"src="hero1.png"></img>
+      </div>
+
       {/* step 01 image section */}
+      <div className="lg:w-[50%] lg:flex lg:flex-col lg:justify-center lg:items-center">
+        <div>
+          <p className="text-center text-xl font-semibold pt-5">Step 03</p>
+          </div>
+        <div>
+          <p className="text-center text-3xl font-semibold pt-5 lg:text-right">Collaborate <span className="text-[#FCA311]">With Your</span> Team.</p>
+          <p className="text-center pt-3 lg:text-right">Thanks to cloud-based automatic syncing, your team can make inventory updates from any device—in the office, in the field, anywhere. Advanced user permissions allow you to control who has access to what.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    {/* Step 04 Section */}
+
+    <div className="mx-5 py-5 md:mx-36 lg:flex lg:flex-row">
+      {/* step 02 word section */}
+      <div className="lg:w-[50%] lg:flex lg:flex-col lg:justify-center lg:items-center">
+        <div>
+          <p className="text-center text-xl font-semibold pt-5">Step 04</p>
+          </div>
+        <div>
+          <p className="text-center text-3xl font-semibold pt-5 lg:text-left">Gather <span className="text-[#FCA311]">powerful</span> reporting insights.</p>
+          <p className="text-center pt-3 lg:text-left">Generate, export, and print inventory reports in BoxStock to help you re-order stock, monitor transactions, and better understand your business.</p>
+        </div>
+      </div>
+
+      {/* step 02 image section */}
       <div className="flex justify-center items-center lg:w-[50%]">
         <img className="w-[50%]"src="hero1.png"></img>
       </div>
       
+    </div>
+
+    {/* end of step section */}
+
+    {/* start feature section */}
+
+    <div className="bg-[#EEEBEB]">
+      <div className="mx-5 py-10 md:mx-36">
+        <div>
+          <p className="text-center text-4xl font-semibold">BoxStock Features</p>
+        </div>
+      </div>
+
+      <div className=" mx-5 py-5 md:mx-36 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4 ">
+
+        <div>
+          <div className="bg-[#FCA311] rounded-full w-20 h-20 flex justify-center items-center text-center">
+            <img src="/file4.png"></img>
+            </div>
+        </div>
+        <div className="bg-blue-500">2</div>
+        <div className="bg-blue-500">3</div>
+        <div className="bg-blue-500">4</div>
+        <div className="bg-blue-500">5</div>
+        <div className="bg-blue-500">6</div>
+        <div className="bg-blue-500">7</div>
+        <div className="bg-blue-500">8</div>
+        
+
+
+
+      </div>
+
+
     </div>
 
 
