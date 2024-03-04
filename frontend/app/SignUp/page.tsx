e774644715f73f89d899919d0e996d3a33c84e9e
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-03-04 12:05:20 +0530

    SignUp section Updated New

--- frontend/app/SignUp/page.tsx
+++ frontend/app/SignUp/page.tsx
@@ -4,85 +4,21 @@ import line from '../Line 1.png'
 import google from '../google.png'
 import ios from '../ios.png'
 import Link from 'next/link';
-
-const page = () => {
-  return (
-    <div className='loginForm'>
-      <form action=''>
-
-        <Image className=' flex justify-center items-center'
-          src={logo}
-          width={250}
-          height={250}
-          alt="Picture"
-        />
-
-
-        <div className="text-2xl">
-           <p> Access and manage your instances from this BoxStock account.</p>
-        </div>
-
-
-        <div className="signDetails grid">
-            
-            <div className="input-box grid">
-                <label className='label'>Email</label>
-                <input type="text" placeholder='Email' required/>
-          
-                <label className='label'>Password</label>
-                <input type="password" placeholder='Password' required/>
-            </div>
-
-
-
-        </div>
-
-
-        
-        
-        <div className="">
-        <Image className='pt-1'
-          src={line}
-          width={50}
-          height={50}
-          alt="Line icon"
-        />
-        </div>
-        <h3>Easy Sign In</h3>
-
-        <div className="flex justify-center">
-          <Image className='pt-1'
-            src={google}
-            width={80}
-            height={80}
-            alt="Google icon"
-          />
-
-          <Image className=''import Image from 'next/image';
-import logo from '../BoxStock.png';
-import bg from './bg.png';
 import show from './show.png'
-import line from '../Line 1.png'
-import google from '../google.png'
-import ios from '../ios.png'
-import Link from 'next/link';
-
 
 
 
 const Login = () => {
   return (
-    <div className="flex  flex-col w-full h-[100vh] bg-red-200">
+    <div className="flex  flex-col w-full h-[100vh] bg-red-200 fixed">
       
-      
-          
               <div className="flex justify-center items-center ">
                 <div className="items-center justify-center flex flex-col m-[80px] w-[500px] pl-2 border-[2px] border-red-100 border-solid rounded-[25px] bg-[#ECECEC]">
-                  <form action='' className="p-8 pl-0 leading-normal " >
+                  <form action='' className="p-1 pl-0 leading-normal " >
                  
                  
                     <div className="flex justify-center">
-                      <Image className=' flex flex-col  items-center pl-[50px]'
+                      <Image className=' flex flex-col mt-3 items-center pl-[50px]'
                         src={logo}
                         width={250}
                         height={250}
@@ -94,20 +30,24 @@ const Login = () => {
                     </div>
 
                   
+                  {/* text box created */}
 
-                  <div className="flex flex-col items-center pl-5 m-3">
-                    <div className="w-[400px] flex border border-black rounded-[15px] bg-white pl-4">
-                      <p className="text-[14px]  text-black p-1 m-3"> Access and manage your instances from this BoxStock account.</p>
+                  <div className="flex flex-col items-center pl-0 m-4 p-2">
+                    <div className="w-[400px] flex border border-black rounded-[15px] bg-white pl-6">
+                      <p className="text-[16px]  text-black p-1 m-1"> Access and manage your instances from <br/> <span className="pl-20"> this BoxStock account.</span></p>
                     </div>
                   </div>
                   
 
 
-                  <div className="w-[500px] flex flex-col p-4 m-4 relative ">
+                  <div className="w-[500px] flex flex-col pr-6 m-3 relative ">
                       
                       <div className="flex flex-col">
-                          <label className="font-bold text-xl text-[#14213D] ml-8">Email</label>
-                          <input type="text" placeholder='Email' required className="rounded-[10px] mt-3 ml-8 border border-[#FCA311] p-5 text-sm "/>
+                          <label className="font-bold text-xl text-[#14213D] ml-8">Your Email</label>
+                          <input type="text" placeholder='Email' required className="rounded-[10px] mt-2 ml-8 border border-[#FCA311] p-4 text-sm "/>
+
+                          <label className="font-bold text-xl text-[#14213D] ml-8 mt-4">Your Name</label>
+                          <input type="text" placeholder='Eg. John Doe' required className="rounded-[10px] mt-2 ml-8 border border-[#FCA311] p-4 text-sm "/>
                     
                           <label  className="font-bold text-xl text-[#14213D] mt-5 ml-8 ">Password</label>
                           <div className="flex ml-8 mt-2 outline-none rounded-[10px] gap-[320px] border border-[#FCA311] bg-white">
@@ -117,6 +57,7 @@ const Login = () => {
                               height={30}
                               alt='show logo'
                               className="-ml-[200px]"/>
+                            
 
                           </div>
                           
@@ -125,7 +66,7 @@ const Login = () => {
 
 
                       <div className="text-center justify-center flex p-4 mt-5">
-                       <button className="flex bg-[#14213D] p-3 ml-5 rounded-lg w-[600px] text-2xl text-[#FCA311] items-center justify-center font-extrabold">SIGN IN</button>
+                       <button className="flex bg-[#14213D] p-3 ml-6 rounded-lg w-[600px] text-2xl text-[#FCA311] items-center justify-center font-extrabold">SIGN UP</button>
                       </div>
 
 
@@ -137,7 +78,7 @@ const Login = () => {
 
                   
                   
-                  <div className="ml-[70px] items-center justify-center">
+                  <div className="ml-[60px] items-center justify-center">
                   <Image className='mt-2 leading-3'
                     src={line}
                     width={400}
@@ -145,27 +86,13 @@ const Login = () => {
                     alt="Line icon"
                   />
                   </div>
-                  <h3 className=" flex text-black text-sm justify-center items-center leading-9">Easy Sign In</h3>
 
-                  <div className="flex justify-center">
-                    <Image className="text-center justify-center flex text-2xl"
-                      src={google}
-                      width={80}
-                      height={80}
-                      alt="Google icon"
-                    />
 
-                    <Image className=''
-                      src={ios}
-                      width={80}
-                      height={80}
-                      alt="IOS icon"
-                    />
-                  </div>
+                 
 
-                  <div className="flex items-center justify-center flex-row gap-5">
-                      <h3>First time on BoxStock?</h3>
-                      <Link href="/SignUp">Sign Up</Link>
+                  <div className="flex items-center justify-center flex-row gap-5 mt-2 pt-4 pb-5">
+                      <h3>Already have an account?</h3>
+                      <Link href="/Login">Sign In</Link>
                   </div>
                 </form>
               </div>
@@ -179,23 +106,3 @@ const Login = () => {
 
 export default Login
 
-
-            src={ios}
-            width={80}
-            height={80}
-            alt="IOS icon"
-          />
-        </div>
-
-        <div className="bottom flex">
-            <h3>First time on BoxStock?</h3>
-            <Link href="/SignUp">Sign Up</Link>
-        </div>
-      </form>
-    </div>
-  )
-}
-
-
-
-export default page
